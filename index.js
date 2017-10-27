38b9ec5e63ded51196963515f230ce1551552a2f
Zizaco Zizuini <zizaco@gmail.com>
2017-10-27 16:15:57 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+'use strict'
+
+module.exports = {
+  errors: {
+    ResourceNotFoundError: require('./lib/error/resourceNotFoundError'),
+    ValidationError: require('./lib/error/validationError')
+  },
+  http: {
+    catchError: require('./lib/http/catchError'),
+    HttpResponseBuilder: require('./lib/http/httpResponseBuilder')
+  },
+  middleware: {
+    LoggingHandler: require('./lib/middleware/loggingHandler'),
+    UserTokenMiddleware: require('./lib/middleware/userTokenMiddleware')
+  },
+  model: {
+    sanitizors: {
+      ToIntOptionalSanitizor: require('./lib/model/sanitizors/toIntOptionalSanitizor')
+    },
+    validators: {
+      Base64Validator: require('./lib/model/validators/base64Validator'),
+      DateValidator: require('./lib/model/validators/dateValidator'),
+      DurationValidator: require('./lib/model/validators/durationValidator'),
+      ObjectIdValidator: require('./lib/model/validators/objectIdValidator'),
+      OfTypeValidator: require('./lib/model/validators/ofTypeValidator'),
+      ValidationHelper: require('./lib/model/validators/validationHelper')
+    },
+    BaseValidator: require('./lib/model/baseValidator'),
+    Entity: require('./lib/model/entity')
+  },
+  test: {
+    ApiRequest: require('./lib/test/apiRequest')
+  }
+}
