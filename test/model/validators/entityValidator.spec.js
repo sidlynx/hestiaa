bd4f687a524d3ed74938af258498a460913c80f9
Yahia Brahimi <yahiabrahimi@gmail.com>
2018-01-11 11:22:16 +0100

    1f52lObZ - Refactored tests and improved them, refactored implementation

--- test/model/validators/entityValidator.spec.js
+++ test/model/validators/entityValidator.spec.js
@@ -8,61 +8,53 @@ describe('model/validators/entityValidator', () => {
   afterEach(() => sinon.restore())
 
   it('Should pass in valid entity', async () => {
-    let isValidStub = sinon.stub(entity, 'isValid')
-    isValidStub.resolves(true)
+    let isValidStub = sinon.stub(entity, 'isValid').resolves(true)
+    let resolvedStub = sinon.stub()
+
     await validate(entity,
             'THE ERROR MESSAGE',
-            () => {},
+            resolvedStub,
             (s) => expect().fail(s),
             ['Entity']
           )
     expect(isValidStub.called).to.be(true)
+    expect(resolvedStub.withArgs('entity is valid').called).to.be(true)
   })
 
   it('Should pass in valid entity without specifying type', async () => {
-    let isValidStub = sinon.stub(entity, 'isValid')
-    isValidStub.resolves(true)
+    let isValidStub = sinon.stub(entity, 'isValid').resolves(true)
+    let resolvedStub = sinon.stub()
     await validate(entity,
             'THE ERROR MESSAGE',
-            () => {},
+            resolvedStub,
             (s) => expect().fail(s)
           )
     expect(isValidStub.called).to.be(true)
+    expect(resolvedStub.withArgs('entity is valid').called).to.be(true)
   })
 
   it('Should reject wrong entity type', async () => {
-    let isValidStub = sinon.stub(entity, 'isValid')
-    isValidStub.resolves(true)
-    let expectedError
+    let rejectedStub = sinon.stub()
 
-    try {
-      await validate(entity,
-              'THE ERROR MESSAGE',
-              () => {},
-              (s) => expect().fail(s),
-              ['WrongEntity']
-            )
-    } catch (err) {
-      expectedError = err
-    }
-    expect(expectedError).not.to.be(undefined)
+    await validate(entity,
+            'THE ERROR MESSAGE',
+            (s) => expect().fail(s),
+            rejectedStub,
+            ['WrongEntity']
+          )
+    expect(rejectedStub.withArgs('this entity type doesn\'t exists').called).to.be(true)
   })
 
   it('Should reject if entity is not valid', async () => {
-    let isValidStub = sinon.stub(entity, 'isValid')
-    isValidStub.throws(new Error())
-    let expectedError
+    let rejectedStub = sinon.stub()
+    sinon.stub(entity, 'isValid').rejects()
 
-    try {
-      await validate(entity,
-              'THE ERROR MESSAGE',
-              () => {},
-              (s) => expect().fail(s),
-              ['WrongEntity']
-            )
-    } catch (err) {
-      expectedError = err
-    }
-    expect(expectedError).not.to.be(undefined)
+    await validate(entity,
+            'THE ERROR MESSAGE',
+            (s) => expect().fail(s),
+            rejectedStub,
+            ['Entity']
+          )
+    expect(rejectedStub.withArgs('THE ERROR MESSAGE').called).to.be(true)
   })
 })
